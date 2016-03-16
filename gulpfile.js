14da30658a99132bbab1c8bafaf4325b38fa5fa8
Dan Rathbone <rathbone1200cc@gmail.com>
2016-03-16 11:46:21 -0700

    initial capture/replay script + gulpfile, package.json.  Needs README and tests

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * Gulpfile
+ */
+var gulpfileHelper = require('repo-standards').gulpfileHelper;
+gulpfileHelper.init({
+  lintEnv: 'es6'
+});
---
+++ index.js
@@ -0,0 +1,54 @@
+/* globals sinon */
+'use strict';
+
+require('repo-standards').exposeTestGlobals();
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+const captureDir = 'test/data';
+
+const modes = {
+  live: 'live',
+  capture: 'capture',
+  replay: 'replay'
+};
+const mode = modes[process.env.TEST_MODE || modes.replay];
+
+exports.wrapAsyncMethod = function (obj, method, bucket) {
+  const originalFn = obj[method];
+
+  const stub = sinon.stub(obj, method, function () {
+    const callingArgs = Array.apply(null, arguments);
+    const self = this;
+
+    if (mode === modes.live) { // no fixtures, no problems
+      return originalFn.apply(self, callingArgs);
+    }
+
+    const argStr = JSON.stringify(callingArgs, null, '  ');
+    const hashKey = crypto.createHash('sha256').update(argStr).digest('hex');
+    const argPath = path.join(captureDir, bucket, hashKey + '-args.json');
+    const responsePath = path.join(captureDir, bucket, hashKey + '-response.json');
+    const callback = callingArgs[1];
+    if (mode === modes.capture) {
+      fs.writeFileSync(argPath, argStr);
+      originalFn(callingArgs[0], function () {
+        const callbackArgs = Array.apply(null, arguments);
+        fs.writeFileSync(
+          responsePath,
+          JSON.stringify(callbackArgs, null, '  '));
+        callback.apply(self, callbackArgs);
+      });
+      return;
+    }
+
+    // mode is replay
+    const cannedResponse = fs.readFileSync(responsePath);
+    const cannedJson = JSON.parse(cannedResponse);
+    process.nextTick(() => {
+      callback.apply(self, cannedJson);
+    });
+  });
+  return stub;
+};
+
