e397787452d7cb85960a29291cafb9ba4030c0aa
Dan Rathbone <rathbone1200cc@gmail.com>
2016-03-23 13:11:04 -0700

    attempt to follow expected this/self assignment

--- index.js
+++ index.js
@@ -6,17 +6,35 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 
+let sandbox = sinon.sandbox.create();
+
 const modes = {
   live: 'live',
   capture: 'capture',
   replay: 'replay'
 };
 
+exports.restore = function () {
+  if (sandbox) {
+    sandbox.restore();
+  }
+  sandbox = sinon.sandbox.create();
+};
+
 exports.wrapAsyncMethod = function (obj, method, optionsArg) {
+  sandbox = sandbox || sinon.sandbox.create();
   const originalFn = obj[method];
   const options = {};
   options.bucket = typeof optionsArg === 'string' ? optionsArg : optionsArg.bucket || 'test/data';
   options.mode = optionsArg.mode || modes[process.env.TEST_MODE || modes.replay];
+  options.callbackSwap = optionsArg.callbackSwap || function (args, newCallback) {
+    const origCallback = args[args.length - 1];
+    args[args.length - 1] = newCallback;
+    return origCallback;
+  };
+  options.argumentSerializer = optionsArg.argumentSerializer || function (args) {
+    return JSON.stringify(args, null, '  ');
+  };
 
   const stub = sinon.stub(obj, method, function () {
     const callingArgs = Array.apply(null, arguments);
@@ -26,21 +44,21 @@ exports.wrapAsyncMethod = function (obj, method, optionsArg) {
       return originalFn.apply(self, callingArgs);
     }
 
-    const argStr = JSON.stringify(callingArgs, null, '  ');
+    const argStr = options.argumentSerializer(callingArgs);
     const hashKey = crypto.createHash('sha256').update(argStr).digest('hex');
     const argPath = path.join(options.bucket, hashKey + '-args.json');
     const responsePath = path.join(options.bucket, hashKey + '-response.json');
     // REFACTOR: determine how to generate nicer file names.
-    const callback = callingArgs[callingArgs.length - 1];
+    const origCallback = options.callbackSwap.apply(self, [callingArgs, function () {
+      const callbackArgs = Array.apply(null, arguments);
+      fs.writeFileSync(
+        responsePath,
+        JSON.stringify(callbackArgs, null, '  '));
+      origCallback.apply(this, callbackArgs);
+    }]);
+
     if (options.mode === modes.capture) {
       fs.writeFileSync(argPath, argStr);
-      callingArgs[callingArgs.length - 1] = function () {
-        const callbackArgs = Array.apply(null, arguments);
-        fs.writeFileSync(
-          responsePath,
-          JSON.stringify(callbackArgs, null, '  '));
-        callback.apply(self, callbackArgs);
-      };
       originalFn.apply(self, callingArgs);
       return;
     }
@@ -49,7 +67,7 @@ exports.wrapAsyncMethod = function (obj, method, optionsArg) {
     const cannedResponse = fs.readFileSync(responsePath);
     const cannedJson = JSON.parse(cannedResponse);
     process.nextTick(() => {
-      callback.apply(self, cannedJson);
+      origCallback.apply(self, cannedJson);
     });
   });
   return stub;
