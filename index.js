8a7123dc45b9d41d045997e5e52fa2946cb67fe6
Dan Rathbone <rathbone1200cc@gmail.com>
2016-03-23 18:48:13 -0700

    nicer filenames

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ exports.wrapAsyncMethod = function (obj, method, optionsArg) {
   sandbox = sandbox || sinon.sandbox.create();
   const originalFn = obj[method];
   const options = {};
-  options.bucket = typeof optionsArg === 'string' ? optionsArg : optionsArg.bucket || 'test/data';
+  options.dir = typeof optionsArg === 'string' ? optionsArg : optionsArg.dir || 'test/data';
+  options.prefix = optionsArg.prefix || method;
   options.mode = optionsArg.mode || modes[process.env.TEST_MODE || modes.replay];
   options.callbackSwap = optionsArg.callbackSwap || function (args, newCallback) {
     const origCallback = args[args.length - 1];
@@ -45,9 +46,10 @@ exports.wrapAsyncMethod = function (obj, method, optionsArg) {
     }
 
     const argStr = options.argumentSerializer(callingArgs);
-    const hashKey = crypto.createHash('sha256').update(argStr).digest('hex');
-    const argPath = path.join(options.bucket, hashKey + '-args.json');
-    const responsePath = path.join(options.bucket, hashKey + '-response.json');
+    const hashKey = crypto.createHash('sha256').update(argStr).digest('hex').slice(0, 12);
+    const basePath = path.join(options.dir, options.prefix + '-' + hashKey);
+    const argPath = basePath + '-args.json';
+    const responsePath = basePath + '-response.json';
     // REFACTOR: determine how to generate nicer file names.
     const origCallback = options.callbackSwap.apply(self, [callingArgs, function () {
       const callbackArgs = Array.apply(null, arguments);
